b551e1fd6fc373c337f080a580ee3ba2308ce6e8
Thomas Beer <thomas.beer04@outlook.com>
2023-06-17 00:54:59 +0100

    filtering using deep_pick

--- bin/reddit-2-video.dart
+++ bin/reddit-2-video.dart
@@ -1,11 +1,11 @@
 import 'package:reddit_2_video/reddit_2_video.dart' as reddit_2_video;
 import '../lib/cmd.dart';
 
+// remove any pinned posts/comments from selection ['stickied']
+
 void main(List<String> arguments) async {
   var results = parse(arguments);
   if (results != null) {
-    var data = await reddit_2_video.getArticleInfo(
-        results['subreddit'], results['sort']);
-    print(data);
+    await reddit_2_video.getArticleInfo(results['subreddit'], results['sort']);
   }
 }
--- lib/reddit_2_video.dart
+++ lib/reddit_2_video.dart
@@ -1,19 +1,27 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:deep_pick/deep_pick.dart';
 
-Future<Map> getArticleInfo(subreddit, sort) async {
+Future<List> getArticleInfo(subreddit, sort) async {
+  // make the network request
   var client = http.Client();
-  late Map data;
+  late final json;
   try {
     var response =
         await client.get(Uri.https("reddit.com", "/r/$subreddit/$sort.json"));
-    data = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+    json = jsonDecode(utf8.decode(response.bodyBytes));
   } catch (e) {
     // do error handling
   } finally {
     client.close();
   }
+  List<dynamic> data = pick(json, 'data', 'children').asListOrEmpty((p0) {
+    print(p0);
+    return p0('data').required().asString();
+  });
+  print(data);
+  print(data.length);
   return data;
 }
 
