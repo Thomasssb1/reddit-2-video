62bcd230ff2ab45a09d748d817bd83bebcff1139
Thomas Beer <thomas.beer04@outlook.com>
2023-07-12 23:08:58 +0100

    Removed unnecessary options & flags

--- lib/cmd.dart
+++ lib/cmd.dart
@@ -9,7 +9,15 @@ ArgResults? parse(args) {
   parser.addOption('sort', defaultsTo: 'hot', abbr: "s", allowed: ['hot', 'new', 'top', 'rising']);
   parser.addOption('comment-sort', defaultsTo: 'top', allowed: ['top', 'best', 'new', 'controversial', 'old', 'q&a']);
   parser.addOption('count', defaultsTo: '8', help: 'Minimum number of comments');
-  parser.addOption('type', defaultsTo: 'comments', allowed: ['comments', 'post', 'multi']);
+  parser.addOption('type', defaultsTo: 'comments', allowed: [
+    'comments',
+    'post',
+    'multi'
+  ], allowedHelp: {
+    'comments': 'Creates a video that contains a post title, body and a set number of comments for that post.',
+    'post': 'Creates a video that only contains a single post with the title and body.',
+    'multi': 'Creates a video that contains multiple posts from a single subreddit, not including comments.'
+  });
   // Look into getting info such as female/male when assigning voices in future
   parser.addMultiOption('alternate',
       valueHelp: "alternate-tts(true/false),alternate-colour(true/false),title-colour(hex)");
@@ -22,19 +30,8 @@ ArgResults? parse(args) {
       defaultsTo: false,
       help:
           'Determines whether to use neural tts which is generated locally or googles own TTS which requires internet.');
-  parser
-    ..addFlag('upvotes', defaultsTo: false, abbr: 'u', help: 'Display upvotes on overlay')
-    ..addFlag('downvotes', defaultsTo: false, abbr: 'd', help: 'Display downvotes on overlay')
-    ..addFlag('awards', defaultsTo: false, abbr: 'a', help: 'Display awards on overlay');
   parser.addOption('video-path', defaultsTo: '../defaults/video1.mp4', abbr: 'v', valueHelp: "path");
   parser.addMultiOption('music', valueHelp: "path,volume");
-  parser.addFlag('date', defaultsTo: false, help: 'Add date of when the post was uploaded to the overlay.');
-  parser.addOption('timezone',
-      abbr: 't',
-      defaultsTo: 'GMT',
-      hide: true,
-      help: 'Timezone to use when adding date to the post overlay',
-      allowed: ['GMT', 'ECT', 'EET', 'PST', 'CST', 'EST']);
   parser.addOption('output', abbr: 'o', defaultsTo: 'final', help: 'Location where the generated file will be stored.');
   parser.addOption('file-type', defaultsTo: 'mp4', allowed: ['mp4', 'avi', 'mov', 'flv']);
   parser.addOption('framerate',
@@ -43,13 +40,12 @@ ArgResults? parse(args) {
       help:
           'The framerate used when generating the video - using a higher framerate will take longer and produce a larger file.');
   parser.addFlag('verbose', defaultsTo: false);
-  parser.addOption('help', abbr: 'h', help: 'Provide information on how to use reddit-2-video');
+  parser.addFlag('help');
 
   var results = parser.parse(args);
 
   if (results.wasParsed('help')) {
     printHelp(parser.usage);
-    //printSuccess(parser.usage);
     return null;
   } else if (!results.wasParsed('subreddit')) {
     stderr.writeln('Argument <subreddit> is required. \nUse -help to get more information about usage.');
