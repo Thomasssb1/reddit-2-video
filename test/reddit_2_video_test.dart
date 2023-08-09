1095d24a74d7e259ad9d63a06d5c96baa1d22b89
Thomasssb1 <thomas.beer04@outlook.com>
2023-08-09 21:26:03 +0100

    Added default test cases for correct data parsing

--- test/reddit_2_video_test.dart
+++ test/reddit_2_video_test.dart
@@ -1,2 +1,25 @@
+import 'package:args/args.dart';
+import 'package:reddit_2_video/cmd.dart';
+import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group("CLI parsing", (){
+    test("Run reddit-2-video command with subreddit arg only", (){
+      var results = parse(['--subreddit', 'AskReddit']);
+      ArgResults args = results['args'];
+
+      List<String?> command = [(args.command == null) ? null : args.command.toString(), ...args.arguments];
+
+      expect([null, '--subreddit', 'AskReddit'], command);
+    });
+    test("Run reddit-2-video command with url as a subreddit arg", (){
+      var results = parse(['--subreddit', 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/']);
+      
+      ArgResults args = results['args'];
+
+      List<String?> command = [(args.command == null) ? null : args.command.toString(), ...args.arguments];
+
+      expect([null, '--subreddit', ], 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/');
+    });
+  });
+}
