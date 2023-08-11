36f34ae5201365f668efec95e89031cbe1e41b9d
Thomasssb1 <thomas.beer04@outlook.com>
2023-08-11 21:02:34 +0100

    Link validation group

--- test/reddit_2_video_test.dart
+++ test/reddit_2_video_test.dart
@@ -19,7 +19,17 @@ void main() {
 
       List<String?> command = [(args.command == null) ? null : args.command.toString(), ...args.arguments];
 
-      expect([null, '--subreddit', ], 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/');
+      expect([null, '--subreddit', 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/'], command);
     });
+    
+  });
+  // need to check for if the link is a post or not
+  group("Link validation", (){
+    test("Reddit link directly to a post", (){
+      String link = "https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/";
+      bool isLink = RegExp(r'''(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)''').hasMatch(link);
+      expect(true, isLink);
+    });
+    //test("Link to a general reddit post", (){});
   });
 }
