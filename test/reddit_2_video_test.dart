ea7e1c769b73d5c3f53fc869dbe38c311872dbfd
Thomas Beer <thomas.beer04@outlook.com>
2023-08-15 23:37:51 +0100

    Reddit post link test

--- test/reddit_2_video_test.dart
+++ test/reddit_2_video_test.dart
@@ -1,10 +1,11 @@
 import 'package:args/args.dart';
 import 'package:reddit_2_video/cmd.dart';
+import 'package:reddit_2_video/utils.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("CLI parsing", (){
-    test("Run reddit-2-video command with subreddit arg only", (){
+  group("CLI parsing", () {
+    test("Run reddit-2-video command with subreddit arg only", () {
       var results = parse(['--subreddit', 'AskReddit']);
       ArgResults args = results['args'];
 
@@ -12,24 +13,22 @@ void main() {
 
       expect([null, '--subreddit', 'AskReddit'], command);
     });
-    test("Run reddit-2-video command with url as a subreddit arg", (){
-      var results = parse(['--subreddit', 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/']);
-      
+    test("Run reddit-2-video command with url as a subreddit arg", () {
+      var results =
+          parse(['--subreddit', 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/']);
+
       ArgResults args = results['args'];
 
       List<String?> command = [(args.command == null) ? null : args.command.toString(), ...args.arguments];
 
-      expect([null, '--subreddit', 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/'], command);
+      expect(
+          [null, '--subreddit', 'https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/'], command);
     });
-    
   });
-  // need to check for if the link is a post or not
-  group("Link validation", (){
-    test("Reddit link directly to a post", (){
-      String link = "https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/";
-      bool isLink = RegExp(r'''(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)''').hasMatch(link);
-      expect(true, isLink);
+  group("Link validation", () {
+    test("Reddit link directly to a post", () {
+      bool isValidLink = validateLink('https://www.reddit.com/r/AskReddit/comments/15lfece/why_did_you_get_fired/');
+      expect(true, isValidLink);
     });
-    //test("Link to a general reddit post", (){});
   });
 }
