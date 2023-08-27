6a4584434034e7ef5d6625455f836c7da5542472
Thomasssb1 <thomas.beer04@outlook.com>
2023-08-27 01:52:07 +0100

    Change to absolute path

--- lib/utils.dart
+++ lib/utils.dart
@@ -140,14 +140,14 @@ bool checkStatusCode(
 /// clear all of tts files inside of .temp/tts and clear any data in temporary files
 void clearTemp() async {
   // get the tts folder and delete it
-  final Directory tempTTSFolder = Directory("./.temp/tts");
+  final Directory tempTTSFolder = Directory("$prePath/.temp/tts");
   await tempTTSFolder.delete(recursive: true).then((_) => null).catchError((error) {
     printError(
         "Unable to clear the temporary data. This will not affect future video generation but will cause ~a few MB stored in the temporary folder. Error: $error");
     return;
   });
   // make a new tts folder that is empty
-  await (Directory('./.temp/tts').create().then((_) => null).catchError((error) {
+  await (Directory('$prePath/.temp/tts').create().then((_) => null).catchError((error) {
     printError(
         "Unable to create TTS folder. If this continues, then post this as an issue on github error https://github.com/Thomasssb1/reddit-2-video/issues along with steps to reproduce this issue. Error: $error");
     return;
@@ -192,7 +192,7 @@ Future<bool> installFFmpeg() async {
 
       if (chocoInstalled) {
         printSuccess("Attempting to use choco to install ffmpeg.");
-        final process = await Process.start('choco', ['install', 'ffmpeg']);
+        final process = await Process.start('choco', ['install', 'ffmpeg', '-y']);
         process.stdout.transform(utf8.decoder).listen((data) {
           stdout.write(data);
         });
