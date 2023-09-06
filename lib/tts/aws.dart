580806c04e593180c1b3dfc4dcc087a890a7821e
Thomasssb1 <thomas.beer04@outlook.com>
2023-09-06 15:08:04 +0100

    Swap to aws polly for TTS

---
+++ lib/tts/aws.dart
@@ -0,0 +1,30 @@
+import 'package:reddit_2_video/utils/prepath.dart';
+import 'package:reddit_2_video/utils/prettify.dart';
+import 'package:reddit_2_video/utils/run.dart';
+
+generateTTS(String text, int counter, bool ntts, String voice) async {
+  var ttsResult = await runCommand(
+      'aws',
+      [
+        'polly',
+        'synthesize-speech',
+        '--output-format',
+        'mp3',
+        '--voice-id',
+        voice,
+        '--text',
+        text,
+        '--engine',
+        ntts ? "neural" : "standard",
+        '.temp/tts/tts-${counter}.mp3',
+      ],
+      true,
+      prePath);
+  if (ttsResult == 0) {
+    printSuccess("\rSuccessfully generated TTS. (${counter + 1}).");
+    return true;
+  } else {
+    printError("\rTTS could not be successfully generated. Exiting.");
+    return false;
+  }
+}
