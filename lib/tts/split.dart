deea6dfb0d96dfa131f3e766ddaaf760d7526689
Thomasssb1 <thomas.beer04@outlook.com>
2023-09-07 01:44:29 +0100

    Logical splitting of text using punctuation

--- lib/tts/split.dart
+++ lib/tts/split.dart
@@ -1,27 +1,23 @@
-int maxLength = 600;
+// need to figure out what happens if the segment contains no punctuation
 
-// need to figure out what happens if the segment contains no bullet points
-
-/*
-List<String> splitText(String text){
+int maxLength = 3000;
+List<String> splitText(String text) {
   List<String> newText = [];
   RegExp punctuationMatch = RegExp(r"(\.|\?|!|\u2026)");
-  List<dynamic> endPoints = punctuationMatch.allMatches(text).expand<int>((e) => [e.end]).toList();
+  List<dynamic> endPoints =
+      punctuationMatch.allMatches(text).expand<int>((e) => [e.end]).toList();
   endPoints.insert(0, 0);
   int prevIndex = 0;
-  for (int i = 0; i < endPoints.length; i++){
-    if (endPoints[i] - endPoints[prevIndex] >= maxLength){
-      newText.add(text.substring(endPoints[prevIndex], endPoints[i-1]));
-      prevIndex = i;
-    }else if (i == endPoints.length-1){
-      newText.add(text.substring(endPoints[prevIndex], text.length));
+  for (int i = 0; i < endPoints.length; i++) {
+    print(endPoints[i]);
+    print(endPoints[i] - endPoints[prevIndex]);
+    if (endPoints[i] - endPoints[prevIndex] >= maxLength) {
+      newText.add(text.substring(endPoints[prevIndex], endPoints[i - 1]));
+      prevIndex = i - 1;
+    } else if (i == endPoints.length - 1) {
+      newText.add(text.substring(endPoints[prevIndex], endPoints[i]));
+      newText.add(text.substring(endPoints[i], text.length));
     }
   }
   return newText;
-}*/
-
-List<String> splitText(String text) {
-  int splitAmount = 200;
-  RegExp splitMatch = RegExp('.{1,$splitAmount}');
-  return splitMatch.allMatches(text).map((e) => e[0]!).toList();
 }
